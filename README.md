c0ff9f16479b3525967049b925458e64d4fb7f69
Ruben Romero <romeroa.ruben@gmail.com>
2021-09-08 15:29:21 -0300

    Commit inicial

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
-# actividad_abm_usuarios
+
+# BugTracker - ABM Usuarios
+
+
+## Clonar Repositorio (Clone/Checkout)
+
+**1. Ejecutar comando clone para descargar repositorio:** 
+```sh
+$ git clone https://github.com/utn-frc-pav1-3k5-2020/actividad_abm_usuarios.git
+```
+**2. Ubicarse en la carpeta generada con el nombre del repositorio: 
+
+```sh
+$ cd actividad_abm_usuarios
+```
+
+**3. Crear un nuevo branch (rama)**
+
+Para crear una nueva rama (branch) y saltar a ella, en un solo paso, puedes utilizar el comando  `git checkout`  con la opción  `-b`, indicando el nombre del nuevo branch (reemplazando el nro de legajo) de la siguiente forma branch_{legajo}, para el legajo 12345:
+
+```sh
+$ git checkout -b branch_12345 
+Switched to a new branch "12345"
+```
+Y para que se vea reflejada en GitHub:
+```sh
+$ git push --set-upstream origin branch_12345
+```
+
+## Actividad ABM Usuarios
+
+> Iniciar la solución que se encuentra en la carpeta src/BugTracker.sln
+Se agregaron 2 formularios. 
+> - frmUsuario: Consulta los usuarios registrados con un filtro por "Nombre" y Perfil (que se elige de un combo)
+> - frmABMUsuario: Permite hacer el Alta y Modificación de usuarios, que previamente se seleccionaron en frmUsuario.
+
+Sobre el formulario `frmABMUsuario` realizar las siguientes actividades:
+
+**1. Validar Campos Obligatorios.**
+En el método ValidarCampos agregar las validaciones que faltan sobre los textbox:
+* txtPassword
+* txtConfirmarPass
+* cboPerfil
+
+**2. Actualizar Usuario.**
+Implementar lógica en BusinessLayer y DataAccessLayer para actualizar los datos de un usuario.
+
+**3. Eliminado lógico de Usuario.**
+El borrado lógico consiste en utilizar una columna de la tabla de Usuarios (borrado: true/false) como una marca que indica si fue eliminado o no un elemento. 
+
+
+
+## Versionar en GitHub los cambios locales (add / commit / push)
+
+> A continuación vamos a crear el **commit** y subir los cambios al servidor GitHub.
+
+1. **Status**. Verificamos los cambios pendientes de versionar.
+
+```sh
+$ git status
+```
+
+2. **Add**. Agregamos todos los archivos nuevos no versionados.
+
+```sh
+$ git add -A
+```
+
+3. **Commit**. Generamos un commit con todos los cambios y agregamos un comentario.
+
+```sh
+$ git commit -a -m "Comentario"
+```
+
+4. **Push**. Enviamos todos los commits locales a GitHub
+
+```sh
+$ git push
+```
+
+5. **Status**. Verificar que no quedaron cambios pendientes de versionar
+
+```sh
+$ git status
+```
---
+++ src/BugTracker/DataAccessLayer/UsuarioDao.cs
@@ -0,0 +1,145 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using BugTracker.Entities;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BugTracker.DataAccessLayer
+{
+    public class UsuarioDao
+    {
+        public IList<Usuario> GetAll()
+        {
+            List<Usuario> listadoBugs = new List<Usuario>();
+
+            String strSql = string.Concat(" SELECT id_usuario, ",
+                                          "        usuario, ",
+                                          "        email, ",
+                                          "        password, ",
+                                          "        p.id_perfil, ",
+                                          "        p.nombre perfil ",
+                                          "   FROM Usuarios u",
+                                          "  INNER JOIN Perfiles p ON u.id_perfil= p.id_perfil " +
+                                          "  WHERE u.borrado = 0 ");
+
+            var resultadoConsulta = DataManager.GetInstance().ConsultaSQL(strSql);
+
+            foreach (DataRow row in resultadoConsulta.Rows)
+            {
+                listadoBugs.Add(ObjectMapping(row));
+            }
+
+            return listadoBugs;
+        }
+        public Usuario GetUser(string nombreUsuario)
+        {
+            //Construimos la consulta sql para buscar el usuario en la base de datos.
+            String strSql = string.Concat(" SELECT id_usuario, ",
+                                          "        usuario, ",
+                                          "        email, ",
+                                          "        password, ",
+                                          "        p.id_perfil, ",
+                                          "        p.nombre perfil ",
+                                          "   FROM Usuarios u",
+                                          "  INNER JOIN Perfiles p ON u.id_perfil= p.id_perfil ",
+                                          "  WHERE usuario = @usuario");
+
+            var parametros = new Dictionary<string, object>();
+            parametros.Add("usuario", nombreUsuario);
+            //Usando el método GetDBHelper obtenemos la instancia unica de DBHelper (Patrón Singleton) y ejecutamos el método ConsultaSQL()
+            var resultado = DataManager.GetInstance().ConsultaSQL(strSql, parametros);
+
+            // Validamos que el resultado tenga al menos una fila.
+            if (resultado.Rows.Count > 0)
+            {
+                return ObjectMapping(resultado.Rows[0]);
+            }
+
+            return null;
+        }
+
+        public IList<Usuario> GetByFilters(Dictionary<string, object> parametros)
+        {
+            List<Usuario> lst = new List<Usuario>();
+            String strSql = string.Concat(" SELECT id_usuario, ",
+                                              "        usuario, ",
+                                              "        email, ",
+                                              "        password, ",
+                                              "        p.id_perfil, ",
+                                              "        p.nombre perfil ",
+                                              "   FROM Usuarios u",
+                                              "  INNER JOIN Perfiles p ON u.id_perfil= p.id_perfil ",
+                                              "  WHERE u.borrado = 0");
+
+            if (parametros.ContainsKey("idPerfil"))
+                strSql += " AND (u.id_perfil = @idPerfil) ";
+
+
+            if (parametros.ContainsKey("usuario"))
+                strSql += " AND (u.usuario LIKE '%' + @usuario + '%') ";
+
+            var resultado = DataManager.GetInstance().ConsultaSQL(strSql, parametros);
+
+            foreach (DataRow row in resultado.Rows)
+                lst.Add(ObjectMapping(row));
+
+            return lst;
+        }
+
+        internal bool Create(Usuario oUsuario)
+        {
+            string str_sql = "     INSERT INTO Usuarios (usuario, password, email, id_perfil, borrado)" +
+                             "     VALUES (@usuario, @password, @email, @id_perfil, 0)";
+
+            var parametros = new Dictionary<string, object>();
+            parametros.Add("usuario", oUsuario.NombreUsuario);
+            parametros.Add("password", oUsuario.Password);
+            parametros.Add("email", oUsuario.Email);
+            parametros.Add("id_perfil", oUsuario.Perfil.IdPerfil);
+
+            // Si una fila es afectada por la inserción retorna TRUE. Caso contrario FALSE
+            return (DataManager.GetInstance().EjecutarSQL(str_sql, parametros) == 1);
+        }
+
+        internal bool Update(Usuario oUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool Delete(Usuario oUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private Usuario ObjectMapping(DataRow row)
+        {
+            Usuario oUsuario = new Usuario
+            {
+                IdUsuario = Convert.ToInt32(row["id_usuario"].ToString()),
+                NombreUsuario = row["usuario"].ToString(),
+                Email = row["email"].ToString(),
+                Password = row.Table.Columns.Contains("password") ? row["password"].ToString() : null,
+                Perfil = new Perfil() {
+                    IdPerfil = Convert.ToInt32(row["id_perfil"].ToString()),
+                    Nombre = row["perfil"].ToString(),
+                }                
+            };
+
+            return oUsuario;
+        }
+    }
+
+}
